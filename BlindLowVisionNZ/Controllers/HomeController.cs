233189e0c2203bbee6b84f2f9f6da85a6ad5ccf1
Abdul Shekhumia <ac98695@avcol.school.nz>
2020-10-26 15:12:25 +1300

    Added home button in navbar and created homepage

--- BlindLowVisionNZ/Controllers/HomeController.cs
+++ BlindLowVisionNZ/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BlindLowVisionNZ.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
