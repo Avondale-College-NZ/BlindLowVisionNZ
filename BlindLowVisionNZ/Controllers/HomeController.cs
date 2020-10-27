fd79e872cd25df3a34e49c8ab38af621034e11f5
Abdul Shekhumia <ac98695@avcol.school.nz>
2020-10-27 21:49:36 +1300

    Added Navbars and changed colour of background

--- BlindLowVisionNZ/Controllers/HomeController.cs
+++ BlindLowVisionNZ/Controllers/HomeController.cs
@@ -33,7 +33,23 @@ namespace BlindLowVisionNZ.Controllers
         {
             return View();
         }
-        
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Notices()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
